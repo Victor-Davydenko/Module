b2719f14388bac20935a731cf2849cfa366049ec
Victor <vitek870918@gmail.com>
2020-04-30 13:15:15 +0300

    mainpage

---
+++ module/src/js/main.js
@@ -0,0 +1,58 @@
+$(".menu-opener").click(function() {
+    $('.burger-menu').toggle();
+});
+$(document).on('click', function(e) {
+    if (!$(e.target).closest(".center-header .container").length) {
+        $('.burger-menu').hide();
+    }
+    e.stopPropagation();
+});
+
+$(document).ready(function(){
+    $('.promo-slider').slick({
+        dots:true,
+        arrows: true,
+        autoplay:false
+    });
+});
+
+$('#star').raty({
+    path: "../images"
+});
+
+$('#star1').raty({
+    path: "../images"
+});
+
+$('#star2').raty({
+    path: "../images"
+});
+
+$('#star3').raty({
+    path: "../images"
+});
+
+$(document).ready(function(){
+    $('.popular__slider').slick({
+        dots:false,
+        arrows: true,
+        autoplay:true,
+        slidesToShow: 4,
+        slidesToScroll:1
+    });
+});
+
+$(document).ready(function(){
+    $('.news__holder').slick({
+        dots:false,
+        arrows: false,
+        autoplay:false,
+        slidesToShow: 1,
+        variableWidth: true
+    });
+});
+
+
+
+
+
